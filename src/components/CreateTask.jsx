6e25836bf7af9d8a7844aeb514f26e4b8e086630
khan faysal <mdfaysalkhancse@gmail.com>
2023-04-19 18:45:22 +0600

    create task input file addedc

--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CreateTask = ({ tasks, setTasks }) => {
     return (
         <form>
-            <input type="text" className='border-2 border-slate-400' />
+            <input type="text" className='border-2 border-slate-400 rounded-md' />
             <button>Create</button>
         </form>
     )
