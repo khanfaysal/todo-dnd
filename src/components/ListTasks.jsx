97583ee77cd3a351ee4199934947a88adc779e05
khan faysal <mdfaysalkhancse@gmail.com>
2023-04-20 02:42:02 +0600

    task status added

--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
-const ListTasks = () => {
+const ListTasks = ({ tasks, setTasks }) => {
+    const [todos, setTodos] = useState([]);
+    const [inProgress, setInProgress] = useState([]);
+    const [closed, setClosed] = useState([]);
+
+    useEffect(() => {
+        const filterTodos = tasks.filter(task => task.status === 'todo');
+        const filterProgress = tasks.filter(task => task.status === 'inProgress');
+        const filterClosed = tasks.filter(task => task.status === 'closed');
+
+        setTodos(filterTodos)
+        setInProgress(filterProgress)
+        setClosed(filterClosed)
+    }, [tasks]);
+
+    const todoStatuses = ['todo', 'inprogress', 'closed'];
+
+
     return (
-        <div>ListTasks</div>
+        <div className='flex gap-16'>
+            {todoStatuses.map((todoStatus, index) => <Section key={index} todoStatus={todoStatus} />)}
+        </div>
     )
 }
 
-export default ListTasks
+export default ListTasks;
+
+const Section = ({ todoStatus }) => {
+    return (
+        <>
+            <h2>{todoStatus}</h2>
+        </>
+    )
+}
