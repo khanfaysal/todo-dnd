a814971c64205254eefcdedc23b055e81d04f720
khan faysal <mdfaysalkhancse@gmail.com>
2023-04-20 00:56:45 +0600

    tasks send to createTask component via props and create task object and store in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import CreateTask from './components/CreateTask';
 import ListTasks from './components/ListTasks';
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
+  console.log('tasks', tasks);
+
+  useEffect(() => {
+    setTasks(JSON.parse(localStorage.getItem('tasks')))
+  }, [])
   return (
     <div className='bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16'>
       <CreateTask tasks={tasks} setTasks={setTasks} />
--- src/components/CreateTask.jsx
+++ src/components/CreateTask.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 
 const CreateTask = ({ tasks, setTasks }) => {
+
+    const [task, setTask] = useState({
+        id: '',
+        name: '',
+        status: 'todo'
+    });
+
+    console.log(task);
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        setTasks((prev) => {
+            const list = [...prev, task];
+            localStorage.setItem("tasks", JSON.stringify(list))
+            return list;
+        })
+    }
     return (
-        <form>
-            <input type="text" className='border-2 border-slate-400 rounded-md' />
-            <button>Create</button>
+        <form onSubmit={handleSubmit}>
+            <input
+                type="text"
+                className='border-2 border-slate-400 rounded-md mr-4 h-12 w-64 [x-1'
+                value={task.name}
+                onChange={(e) => setTask({ ...task, id: uuidv4(), name: e.target.value })}
+            />
+            <button className='bg-cyan-500 rounded-md px-4 h-12 text-white'>Create</button>
         </form>
     )
 }
