cc7121740b786522c60a351e1c74f8b7831ad2e6
khan faysal <mdfaysalkhancse@gmail.com>
2023-04-20 18:28:17 +0600

    react dnd package added and implement running

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react'
 import CreateTask from './components/CreateTask';
 import ListTasks from './components/ListTasks';
 import toast, { Toaster } from 'react-hot-toast';
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
 const App = () => {
   const [tasks, setTasks] = useState([]);
@@ -20,13 +22,13 @@ const App = () => {
     }
   }, []);
   return (
-    <>
+    <DndProvider backend={HTML5Backend}>
       <Toaster />
       <div className='bg-slate-100 w-screen h-screen flex flex-col items-center pt-3 gap-16 pt-30'>
         <CreateTask tasks={tasks} setTasks={setTasks} />
         <ListTasks tasks={tasks} setTasks={setTasks} />
       </div>
-    </>
+    </DndProvider>
   )
 }
 
--- src/components/ListTasks.jsx
+++ src/components/ListTasks.jsx
@@ -71,10 +71,11 @@ const Task = ({ task, tasks, setTasks }) => {
     return (
         <div className={`relative p-4 mt-8 shadow-md rounded-md cursor-grab`}>
             <p> {task.name}</p>
-            <button className='absolute bottom-1 right-1 text-slate-400' onClick={() => handleRemove(task.id)}>
+            <button className='absolute bottom-1 right-1 text-red-300' onClick={() => handleRemove(task.id)}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                    <path strokeLinecap="round" strokeLinejoin="round" d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                 </svg>
+
             </button>
 
 
